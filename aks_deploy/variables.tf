ef60950d5dbe6d0408cc1a8b4d9af2e2e562e48a
Angel Alberto Márquez Vargas <angelalberto.marquez@tui.com>
2021-10-10 12:11:43 +0200

    Initial deployment

--- aks_deploy/variables.tf
+++ aks_deploy/variables.tf
@@ -2,39 +2,39 @@ variable "client_id" {}
 variable "client_secret" {}
 
 variable "agent_count" {
-    default = 3
+  default = 3
 }
 
 variable "ssh_public_key" {
-    default = "~/.ssh/id_rsa.pub"
+  default = "~/.ssh/id_rsa.pub"
 }
 
 variable "dns_prefix" {
-    default = "sdgusecase"
+  default = "sdgusecase"
 }
 
-variable cluster_name {
-    default = "akssdgusecase"
+variable "cluster_name" {
+  default = "akssdgusecase"
 }
 
-variable resource_group_name {
-    default = "sdgusecase"
+variable "resource_group_name" {
+  default = "sdgusecase"
 }
 
-variable location {
-    default = "West Europe"
+variable "location" {
+  default = "West Europe"
 }
 
-variable log_analytics_workspace_name {
-    default = "lawsdgusecase"
+variable "log_analytics_workspace_name" {
+  default = "lawsdgusecase"
 }
 
 # refer https://azure.microsoft.com/global-infrastructure/services/?products=monitor for log analytics available regions
-variable log_analytics_workspace_location {
-    default = "westeurope"
+variable "log_analytics_workspace_location" {
+  default = "westeurope"
 }
 
 # refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing 
-variable log_analytics_workspace_sku {
-    default = "PerGB2018"
+variable "log_analytics_workspace_sku" {
+  default = "PerGB2018"
 }
