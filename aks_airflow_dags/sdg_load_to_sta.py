ffe14fc15fd3e3cf9f005bff2bcfae0122b76ed9
Angel Alberto Márquez Vargas <angelalberto.marquez@tui.com>
2021-10-14 18:56:00 +0200

    Update sdg_load_to_sta.py

--- aks_airflow_dags/sdg_load_to_sta.py
+++ aks_airflow_dags/sdg_load_to_sta.py
@@ -34,7 +34,7 @@ with DAG(
     default_args=args,
     schedule_interval=None,
     start_date=days_ago(2),
-    tags=['example', 'example2'],
+    tags=['SDG', 'USECASE'],
 ) as dag:
 
     affinity = {
@@ -51,16 +51,7 @@ with DAG(
     }
 
     tolerations = [{'key': 'dedicated', 'operator': 'Equal', 'value': 'airflow'}]
-
-    def assert_zip_binary():
-        """
-        Checks whether Zip is installed.
-
-        :raises SystemError: if zip is not installed
-        """
-        return_code = os.system("zip")
-        if return_code != 0:
-            raise SystemError("The zip binary is not found")
+    
 
     # You don't have to use any special KubernetesExecutor configuration if you don't want to
     start_task = PythonOperator(task_id="start_task", python_callable=print_stuff)
@@ -69,35 +60,7 @@ with DAG(
     one_task = PythonOperator(
         task_id="one_task",
         python_callable=print_stuff,
-        executor_config={"KubernetesExecutor": {"image": "airflow/ci:latest"}},
+        executor_config={"KubernetesExecutor": {"image": "docker.pkg.github.com/angelalbertomv/sdg.usecase/sdgusecasedag:1a6b7c6"}},
     )
 
-    # Use the zip binary, which is only found in this special docker image
-    two_task = PythonOperator(
-        task_id="two_task",
-        python_callable=assert_zip_binary,
-        executor_config={"KubernetesExecutor": {"image": "airflow/ci_zip:latest"}},
-    )
-
-    # Limit resources on this operator/task with node affinity & tolerations
-    three_task = PythonOperator(
-        task_id="three_task",
-        python_callable=print_stuff,
-        executor_config={
-            "KubernetesExecutor": {
-                "request_memory": "128Mi",
-                "limit_memory": "128Mi",
-                "tolerations": tolerations,
-                "affinity": affinity,
-            }
-        },
-    )
-
-    # Add arbitrary labels to worker pods
-    four_task = PythonOperator(
-        task_id="four_task",
-        python_callable=print_stuff,
-        executor_config={"KubernetesExecutor": {"labels": {"foo": "bar"}}},
-    )
-
-    start_task >> [one_task, two_task, three_task, four_task]
+    start_task >> one_task
