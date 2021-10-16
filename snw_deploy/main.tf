9a06d33301e18b032ad5988e57c0595d765d0887
Angel Alberto Márquez Vargas <angelalberto.marquez@tui.com>
2021-10-16 10:11:51 +0200

    Update main.tf

--- snw_deploy/main.tf
+++ snw_deploy/main.tf
@@ -25,7 +25,6 @@ resource "snowflake_role" "role" {
 }
 
 resource "snowflake_database_grant" "grant" {
-  provider          = snowflake.security_admin
   database_name     = snowflake_database.db.name
   privilege         = "USAGE"
   roles             = [snowflake_role.role.name]
@@ -33,15 +32,12 @@ resource "snowflake_database_grant" "grant" {
 }
 
 resource "snowflake_schema" "schema" {
-  provider   = snowflake.sys_admin
   database   = snowflake_database.db.name
   name       = "TF_DEMO"
   is_managed = false
 }
 
-
 resource "snowflake_schema_grant" "grant" {
-  provider          = snowflake.security_admin
   database_name     = snowflake_database.db.name
   schema_name       = snowflake_schema.schema.name
   privilege         = "USAGE"
@@ -50,7 +46,6 @@ resource "snowflake_schema_grant" "grant" {
 }
 
 resource "snowflake_warehouse_grant" "grant" {
-  provider          = snowflake.security_admin
   warehouse_name    = snowflake_warehouse.warehouse.name
   privilege         = "USAGE"
   roles             = [snowflake_role.role.name]
@@ -62,9 +57,7 @@ resource "tls_private_key" "svc_key" {
   rsa_bits  = 2048
 }
 
-
 resource "snowflake_user" "user" {
-  provider          = snowflake.security_admin
   name              = "tf_demo_user"
   default_warehouse = data.snowflake_warehouse.warehouse.name
   default_role      = snowflake_role.role.name
@@ -73,7 +66,6 @@ resource "snowflake_user" "user" {
 }
 
 resource "snowflake_role_grants" "grants" {
-  provider  = snowflake.security_admin
   role_name = snowflake_role.role.name
   users     = [snowflake_user.user.name]
 }
