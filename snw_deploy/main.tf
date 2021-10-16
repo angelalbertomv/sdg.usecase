5942de394bf880aa3cbd94d7629ab413c861d723
Angel Alberto Márquez Vargas <angelalberto.marquez@tui.com>
2021-10-16 11:30:34 +0200

    Update main.tf

--- snw_deploy/main.tf
+++ snw_deploy/main.tf
@@ -63,7 +63,7 @@ resource "snowflake_table" "table" {
     name = "LOAD_DATE"
     type = "TIMESTAMP_NTZ(9)"
     default {
-      expression = CURRENT_TIMESTAMP()
+      constant = CURRENT_TIMESTAMP()
     }
   }
 
