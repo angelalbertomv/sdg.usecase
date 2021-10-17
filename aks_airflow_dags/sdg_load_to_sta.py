0cae6c5025bcbe415a302685ad21ad0bcaaff536
Angel Alberto Márquez Vargas <angelalberto.marquez@tui.com>
2021-10-17 17:13:29 +0200

    Update sdg_load_to_sta.py

--- aks_airflow_dags/sdg_load_to_sta.py
+++ aks_airflow_dags/sdg_load_to_sta.py
@@ -26,7 +26,7 @@ args = {
 }
 
 with DAG(
-    dag_id='sdg_test_case_loadsta_v01',
+    dag_id='sdg_test_case_staingestion_v01',
     default_args=args,
     schedule_interval=None,
     start_date=days_ago(2),
