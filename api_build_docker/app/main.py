25555cba5fe2a81e7e6c936f5eddb304900147e8
Angel Alberto Márquez Vargas <angelalberto.marquez@tui.com>
2021-10-18 16:31:09 +0200

    API Snowflake connecting

--- api_build_docker/app/main.py
+++ api_build_docker/app/main.py
@@ -1,6 +1,13 @@
 from typing import Optional
-
+import snowflake.connector
 from fastapi import FastAPI
+
+ctx = snowflake.connector.connect(
+    user='angelalbertomv',
+    password='.Kirschner21',
+    account='kd23129.west-europe.azure'
+    )
+cs = ctx.cursor()
 
 app = FastAPI()
 
@@ -9,7 +16,12 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/shares/{share_id}")
+def read_share(share_id: int, q: Optional[str] = None):
+    try:
+        price = cs.cursor().execute("""select * from "SDG_DB"."SDG_USECASE"."sdl_sdgtestcase" where BUSINESS_ID={0};""".format(share_id)).fetchone()
+    finally:
+        cs.close()    
+    ctx.close()
+    
+    return {"share_id": share_id, "price": price}
