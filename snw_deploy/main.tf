7fa2bc1ccbfd13f88552f89145ed4f0346a0ee3c
Angel Alberto Márquez Vargas <angelalberto.marquez@tui.com>
2021-10-18 07:11:30 +0200

    Update main.tf

--- snw_deploy/main.tf
+++ snw_deploy/main.tf
@@ -18,6 +18,7 @@ locals {
   schema_name    = "SDG_USECASE"
   database_name  = "SDG_DB"
   table_name     = "adl_sdgtestcase"
+  table_name2     = "sdl_sdgtestcase"  
 }
 
 resource "snowflake_database" "db" {
@@ -81,5 +82,37 @@ resource "snowflake_table" "table" {
   primary_key {
     name = "my_key"
     keys = ["LOAD_ID"]
+  }
+}
+
+resource "snowflake_table" "table2" {
+  database        = snowflake_database.db.name
+  schema          = snowflake_schema.schema.name
+  name            = local.table_name2
+  comment         = "SDG TEST CASE"
+  cluster_by      = ["to_date(LOAD_DATE)"]
+  change_tracking = false
+
+  column {
+    name     = "BUSINESS_ID"
+    type     = "text"
+    nullable = false
+  }
+
+  column {
+    name     = "PRICE"
+    type     = "float"
+    nullable = false
+  }
+
+  column {
+    name = "LOAD_DATE"
+    type = "TIMESTAMP_NTZ(9)"
+
+  }
+
+  primary_key {
+    name = "my_key"
+    keys = ["BUSINESS_ID"]
   }
 }
